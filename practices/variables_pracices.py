682c19be9f763f342626997476250c51a4857557
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-02 13:38:38 -0600

    Programmed variables practice

--- practices/variables_pracices.py
+++ practices/variables_pracices.py
@@ -1,2 +1,14 @@
 # JS 6th Variables Practice
 
+name=input("Hello what is your name?:")
+number=input("what is a number between 1 and 100?:")
+choice=input("What is a number between 100 and 1000:")
+Breakfast=input("What did you have for breakfast:")
+color=input("What is your favorite color?:")
+school=input("What is the schools name?:")
+year=input("What is the current year?:")
+eye_color=input("what is your eye color?:")
+age=input("what is your age?:")
+subject=input("what is your favorite subject in school?:")
+
+print("Hello what is your name?, name, What is a number between 1 and 100, number, what is number between 100 and 1000, choice, What did you have for breakfast, breakfast, What is the schools name, school, what is the current year, year, what is your eye color, eye_color, what is your age, age, what is your favorite subject in school, subject")
