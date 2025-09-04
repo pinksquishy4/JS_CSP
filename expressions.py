aa1957b69338e6fb082a2d60c4658262b562a9d5
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-04 13:43:51 -0600

    Started expressions notes

---
+++ expressions.py
@@ -0,0 +1,47 @@
+# JS 6th Expressions Notes
+
+#What is an algorithm?
+# A set of instructions to accomplish a task
+#An expression is a list of numbers put together to create a solution
+name = input("What is your name?")
+print("Hello", name)
+
+length = 5
+width = 20
+area = length * width
+print(length,"+", width, "=", area)
+#Algorithm for area
+
+#List steps in an algorithm
+#First, detirmine what pieces of information are needed (or variables)
+# Second, proccess info
+# Third, provide an output 
+#1
+age_one = 14
+age_two = 14
+age_three = 15
+age_four = 15
+#2
+total= age_one + age_two + age_three + age_four
+average = total/4 
+#3
+print("The average age between:", age_one, "-", age_two, "-", age_three, "-", age_four, "=", average)
+
+
+#List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+num_one = 12
+num_two = 4
+print("addition:", num_one + num_two)
+print("Subtraction:", num_one - num_two)
+print("Multiplication:", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("interger divison:", num_one // num_two)
+print("modulo (remainder):", num_one % num_two)
+#All you have to do for exponents is two stars, for addition the plus, for subtraction the minus, the division the slash, for interger divison, two slashes, and last modulo (remainder) is done with the percent sign %
+
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+#
+
+#Why are expressions so important in programming?
+#
