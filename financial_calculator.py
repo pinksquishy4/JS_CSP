91ec0b4c6fac706bff956bc4268ae1c01c89a1ee
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-08 05:02:54 +0000

    changes to calculator

---
+++ financial_calculator.py
@@ -0,0 +1,9 @@
+#JS 6th financial calculator
+
+income = input("Hello, what is your monthly income?:")
+cost_rent = input("What is your monthly rent/mortgage?:")
+cost_utilities = input("what is your monthly utilites cost?:")
+cost_groceries = input("What is the monthly cost of groceries?:")
+cost_transporation = input("What is the monthly cost of your transportation?:")
+
+
