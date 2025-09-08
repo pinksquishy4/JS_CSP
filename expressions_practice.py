84a193d63b59d946417d080137117bd265f5036a
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-08 04:50:41 +0000

    Added to expressions_practice

--- expressions_practice.py
+++ expressions_practice.py
@@ -1,2 +1,89 @@
 # VL 6th Expressions practice
 
+num_one = 7
+num_two = 24
+num_three = 8
+num_four = 4
+num_five = 6
+total = num_one - num_two / num_three * num_four + num_five
+print("The answer between:", num_one, "/", num_two, "*", num_three, "+", num_four, "+", num_five, "=", total)
+
+num_one = 18
+num_two = 3
+num_three = 7
+num_four = 2
+num_five = 5
+total = num_one / num_two - num_three + num_four * num_five
+print("The answer between:", num_one, "/", num_two, "-", num_three, "+", num_four, "*", num_five, "=", total)
+
+
+num_one = 6
+num_two = 4
+num_three = 12
+num_four = 72
+num_five = 8 
+num_six = 9
+total = num_one * num_two / num_three + num_four/ num_five - num_six
+print("The answer between:", num_one, "*", num_two, "/", num_three, "+", num_four, "/", num_five, "-", num_six, "=", total)
+
+num_one = 17
+num_two = 6
+num_three = 2
+num_four = 4
+num_five = 3
+total = (num_one - num_two / num_three) + num_four * num_five
+print("The answer between:", (num_one, "-", num_two, "/", num_three), + num_four * num_five, "=", total)
+
+
+num_one = -2
+num_two = 1
+num_three = 4
+num_four = 2
+num_five = 2
+num_six = 6
+num_seven = 2
+num_eight = 3
+total = num_one*(num_two * num_three - num_four / num_five) + (num_six + num_seven - num_eight)
+print("The answer between:", num_one, "*", (num_two, "*", num_three, "-", num_four, "/", num_five), "+", (num_six, "+", num_seven,"-", num_eight), "=", total )
+
+num_one= -1
+num_two = 3
+num_three = 4
+num_four = 7
+num_five = 5
+num_six = 2
+num_seven = 24
+num_eight = 6
+total = num_one * ( num_two - num_three * num_four) / num_five - num_six * num_seven / num_eight
+print("The answer between:", num_one, "*", "[", "(", num_two, "-", num_three, "*", num_four, ")", "/", num_five, "]", "*", num_six, "*", num_seven, "/", num_eight, "=", total)
+
+
+num_one = 3
+num_two = 5
+num_three = 2
+num_four = 15
+num_five= 5
+num_six= 2
+num_seven= 2
+total = (num_one * num_two**2 / num_four) - (num_five - num_six**2)
+print("The answer between:", "(", num_one, "*", num_two**2, "/",num_four, ")", "-", "(", num_five, "-", num_six**2, ")", "=", total )
+
+
+num_one = 1
+num_two = 2
+num_three = 3
+num_four = 2
+num_five = 4
+total = (num_one**4 * num_two**2 + num_three**3) - num_four**5 / num_five
+print("The answer between", "(", num_one**4, "*", num_two**2, "+", num_three**3, ")", "-", num_four**5, "/", num_five, "=", total )
+
+
+num_one = 22
+num_two = 2
+num_three = 2
+num_four = 5
+num_five = 4
+num_six = 6
+num_seven = 6
+total = (num_one / num_two - num_three * num_four)**2 + (num_five - num_six / num_seven)**2
+print("The answer between:", (num_one, "/", num_two, "-", num_three, "*", num_four),"**2" , "+", (num_five, "-", num_six, "/", num_seven),"**2", "=", total)
