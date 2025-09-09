34b3745459a65f5026bcf587db6e0c0f411c7e27
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-09 13:41:52 -0600

    added to notes and financial calculator

--- financial_calculator.py
+++ financial_calculator.py
@@ -1,9 +1,11 @@
 #JS 6th financial calculator
 
-income = input("Hello, what is your monthly income?:")
-cost_rent = input("What is your monthly rent/mortgage?:")
-cost_utilities = input("what is your monthly utilites cost?:")
-cost_groceries = input("What is the monthly cost of groceries?:")
-cost_transporation = input("What is the monthly cost of your transportation?:")
+income = float(input("Hello, what is your monthly income?:"))
+cost_rent = float(input("What is your monthly rent/mortgage?:"))
+cost_utilities = float(input("what is your monthly utilites cost?:"))
+cost_groceries = float(input("What is the monthly cost of groceries?:"))
+cost_transporation = float(input("What is the monthly cost of your transportation?:"))
+
+number = ("3853357530")
 
 
--- notes/string_notes.py
+++ notes/string_notes.py
@@ -19,23 +19,36 @@ print("Welcome to my program", name)
 
 #What is debugging?
 #Debugging is fixing problems in my code
+#a bug is any error in my code that keeps it from running or makes it run improperly
 
 #How do you debug the different types of errors?
 #syntax error
+#syntax error is like a grammar error in english class
+#string = "This is my string' (this is a syntax error, because you wrote it wrong) Or if you forget an equal sign or a comma 
+
+
 #Logic error
+#its where our code does something that we dont expect it to do. this is a problem with the steps we took, too complete our process
+
 #run time error
+#when there is a problem in the code that will break the code when it is trying to run
+#It wont pop up any syntex errors, but when we try to run it, it willl break.
 #Describe what each of the methods below does:
 
 #find()
-
+print(sentance.find(brown))
+print(sentance[10:15])
+#So what this is doing is its going to my variable sentance and its going inside and its going to tell me where the word brown is located.
 #concatenate (add)
 first_name = input("What is your first name:\n").strip().title()
 last_name = input("What is your last name:\n").strip().title()
 full_name = first_name + " " + last_name
 print
 #upper()
+#This will make all your lletter you put in uppercase
 name = input("what is your name?:\n").strip().upper()
 #lower()
+#this will make all your letters you type in lowercase
 name = input("What is your name?:\n").strip().lower()
 #strip()
 name = input("what is your name?:\n").strip()
