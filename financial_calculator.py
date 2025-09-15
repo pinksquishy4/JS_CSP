1124ae1f188d12cdb8570961129ee4e20d234662
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-15 02:59:09 +0000

    changes ti assugbmt

--- financial_calculator.py
+++ financial_calculator.py
@@ -1,11 +1,27 @@
 #JS 6th financial calculator
 
 income = float(input("Hello, what is your monthly income?:"))
-cost_rent = float(input("What is your monthly rent/mortgage?:"))
-cost_utilities = float(input("what is your monthly utilites cost?:"))
-cost_groceries = float(input("What is the monthly cost of groceries?:"))
-cost_transporation = float(input("What is the monthly cost of your transportation?:"))
+rent = float(input("What is your monthly rent/mortgage?:"))
+utilities = float(input("what is your monthly utilites cost?:"))
+groceries = float(input("What is the monthly cost of groceries?:"))
+transporation = float(input("What is the monthly cost of your transportation?:"))
 
-number = ("3853357530")
+spending_money = float(income - (rent + utilities + groceries + transporation))
+total_expenses = float(income + rent + utilities + groceries + transporation)
+
+rent_pct = float(rent / income) * 100
+utilities_pct = float(utilities / income) * 100
+groceries_pct = float(groceries / income) * 100
+transporation_pct = float(transporation / income) * 100
+savings = float(income *0.10)
+savings_pct = 10
+
+print("Your rent is $", rent, "and that is", rent_pct, "%, of your income.")
+print("Your utilities are $", utilities, "and that is", utilities_pct, "%, of your income.")
+print("Your groceries are $", groceries, "and that is", groceries_pct, "%, of your income.")
+print("Your transportation is $", transporation, "and that is", transporation_pct, "%, of your income.")
+print("you should save", savings, " a month, and that is", savings_pct, "%, of your income.")
+print("You should have", spending_money, " of money left over for spending.")
+
 
 
