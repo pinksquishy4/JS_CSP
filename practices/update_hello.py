8f7efda3c2eb55a0aceca053abfbfb70a32c3e6a
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-17 13:04:23 -0600

    finished hello world assignment

--- practices/update_hello.py
+++ practices/update_hello.py
@@ -1,7 +1,10 @@
 #JS 6th Update Hello World
 
-def welcome (name):
-    name = input("what is your name")
+def say_hello(name):
     print(f"hello {name}!")
 
-
+say_hello("jane")
+say_hello("juliette")
+say_hello("Wesley")
+say_hello("Isaac")
+say_hello("jaun")
