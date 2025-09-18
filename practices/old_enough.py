5955da57e4cb72c3f4b4dc8518af31298cccc3bf
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-18 13:44:07 -0600

    assignment

---
+++ practices/old_enough.py
@@ -0,0 +1,12 @@
+#JS old enough assignment
+
+age = int(input("Please tell me your age:\n"))
+
+if age >= 18:
+    print("You are old enough to vote!")
+elif age <= 17 and age == 16:
+    print("You are old enough to drive!")
+elif age == 15:
+    print("You are old enough to get a drivers permit.")
+elif age >= 14:
+    print("You are able to go to school")
