71edc0b02b8c7595cc2473a99357579e77dcc1e5
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-19 13:13:49 -0600

    notes change

---
+++ notes/for_loops.py
@@ -0,0 +1,25 @@
+# JS 6th Time and For Loops notes
+import datetime
+
+
+current = datetime.datetime.now()
+hour = current.hour
+print(f"The time is: {current}")
+print(f"The hour is: {hour}")
+
+
+#What is a loop?
+ 
+#What are the two types of loops?
+
+#What is iteration
+
+#What are lists?
+  
+#How do you make lists in python?
+ 
+#How do you make for loops in python? 
+
+#How do you make while loops in python?
+
+
