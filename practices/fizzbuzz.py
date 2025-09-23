f9aab8c4540aa3370fe84a776663df3f8bb1d87d
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-09-23 13:38:30 -0600

    finished assignment

---
+++ practices/fizzbuzz.py
@@ -0,0 +1,14 @@
+#JS 6th fizzbuzz
+
+for i in range (1, 51):
+    if i%3 == 0 and i%5 ==0:
+      print("FizzBuzz")
+    elif i % 3 == 0:
+        print(f"Fizz")
+    elif i % 5 == 0:
+       print(f"Buzz")
+    else:
+       print(i)
+
+
+        
