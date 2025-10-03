6adc7cd9f1cf98dd88756642cd8f3794bcb0b983
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-03 13:43:20 -0600

    lets go girls!

--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -6,43 +6,40 @@ int main(void){
     printf("%d", num);
     float pi = 3.14;
     char grade = 'A'; //will only hold one letter
-    char name[]= "LaRose";
+    char name[]= "Juliette";
+    //bool passing = true;
+
+    printf("What is your letter grade: ");
+    scanf("%c", &grade);
+
+    printf("%d\n", num);
+    printf("%s has a %c grade in the class!\n", name, grade);
 
     return 0;
 }
+//What is the main difference between declaring variables in Python and C?
 
-// What is the main difference between declaring variables in Python and C?
-//C is more efficient with the space, its picky.
+//In C, what is the purpose of specifying a data type when declaring a variable?
 
-// In C, what is the purpose of specifying a data type when declaring a variable?
+//List three common data types used in C and their corresponding format specifiers for printf().
 
-// List three common data types used in C and their corresponding format specifiers for printf().
-//char and it wil only hold one letter
-// float
-// and a list 
-//also a bool
-// c always ends with a semicolon bc thats where it ends thats it.
+//How do you declare and initialize an integer variable named "age" with the value 25 in C?
 
-// How do you declare and initialize an integer variable named "age" with the value 25 in C?
+//What is the difference between printf() and scanf() functions in C?
 
-// What is the difference between printf() and scanf() functions in C?
+//How do you add comments in C?
+// ttwo slashes or astrix and another slash like this
+/*so you ca have it in between*/
 
-// Write a C program that asks the user for their name and age, then prints a greeting message with their information.
+//What is the purpose of the #include <stdio.h> line at the beginning of a C program?
 
-// How do you add comments in C? What are the two types of comments?
+//In C, what is the significance of the main() function?
 
-// What is the purpose of the #include <stdio.h> line at the beginning of a C program?
+//What is the difference between %d and %f format specifiers in printf()?
 
-// In C, what is the significance of the main() function?
+//How do you print a newline character in C?
 
-// Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
+//What is the purpose of the & symbol when using scanf() to get user input?
 
-// What is the difference between %d and %f format specifiers in printf()?
-
-// How do you print a newline character in C?
-
-// Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
-
-// What is the purpose of the & symbol when using scanf() to get user input?
-
-// How do you declare a constant in C? Provide an example.
+//How do you declare a constant in C? Provide an example.
+//you put infront of it the keyword sont so it it cnanot be changed.
