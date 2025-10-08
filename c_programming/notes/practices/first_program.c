6443a487971fd44395a13bb4be0c16b853d718a2
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-08 02:42:01 +0000

    FInsihed last asiignment

---
+++ c_programming/notes/practices/first_program.c
@@ -0,0 +1,13 @@
+// JS 6th First c program
+#include <stdio.h>
+int main(void){
+char name[50];
+printf("Enter your name: ");
+scanf("%s", name);
+   printf("Hello, %s! Nice to meet you.\n", name);
+
+   return 0;
+
+
+
+}
