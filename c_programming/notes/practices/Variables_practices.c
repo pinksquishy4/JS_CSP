af9f2c4688ea07d2008c70a04933844c7a90335c
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-08 02:34:41 +0000

    finished assignments

---
+++ c_programming/notes/practices/Variables_practices.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+char name[] = "Juliette";
+int lucky_number = 4;
+float decimal = 3.14;
+char breakfast[] = "Pancakes";
+char favorite_color[] = "yellow";
+char school_name[] = "UCAS";
+int current_year = 2025;
+char eye_color[] = "brown";
+int age = 14;
+char favorite_subject[] = "math";
+printf("name: %s\n", name);
+printf("lucky number: %d\n", lucky_number);
+printf("decimal: %f\n", decimal);
+printf("breakfast: %s\n", breakfast);
+printf("Favorite color: %s\n", favorite_color);
+printf("School name: %s\n", school_name);
+printf("Current year: %d\n", current_year);
+printf("Eye color: %s\n", eye_color);
+printf("Age: %d\n", age);
+printf("Favorite subject: %s\n", favorite_subject);
+return 0;
+
+
+
+}
