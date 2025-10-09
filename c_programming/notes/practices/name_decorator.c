8c5768ff01de029e12612de1d27b3bf74e0dc908
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-09 13:42:35 -0600

    Finished name decorater

---
+++ c_programming/notes/practices/name_decorator.c
@@ -0,0 +1,21 @@
+// JS 6th Name decorater
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+printf("Pleaste tell me your name: \n");
+char name[50];
+ strcat(name, " ");
+ scanf("%s", name);
+ printf("ʚ%sɞ", name);
+
+
+
+return 0;
+
+
+
+
+
+
+}
