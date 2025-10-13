6f51db41f0c2de9d94beb09f92948acb358c9327
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-13 13:43:42 -0600

    juliette

---
+++ c_programming/practices/Time_of_day.c
@@ -0,0 +1,15 @@
+//Js 6th Time of Day
+#include <stdio.h>
+#include <string.h>
+int main(void){
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ c_programming/practices/hello_family.c
@@ -0,0 +1,9 @@
+// JS 6th My Family
+#include <stdio.h>
+int main(void){
+    char family[5][20] = {"Isabella", "Alexxis", "Jackie", "Gilbert"};
+
+
+
+    return 0;
+}
