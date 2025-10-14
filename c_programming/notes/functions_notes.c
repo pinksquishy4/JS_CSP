10e711e516b8edf23a79dd8fb305ae8d38486e62
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-14 13:41:35 -0600

    notes need to be finished

---
+++ c_programming/notes/functions_notes.c
@@ -0,0 +1,71 @@
+// JS 6th Functions notes
+#include <stdio.h>
+
+void birthday(char* name, int age){
+printf("Happy birthday to you\n");
+printf("Happy birthday to you\n");
+printf("Happy birthday to you\n");
+printf("Happy Birthday dear %s\n", name);
+printf("Happy birthday to you!\n");
+printf("Happy birthfay %s is now %d\n", name, age);   
+
+
+
+
+
+//int add(int num_one, int num_two){
+    //return num_one + nume_two;
+
+//}
+
+}
+//char* get_name(void){
+    //char *name[50];
+    //printf("What is your name?");
+    //scanf("%s", name);
+    //return name;
+    
+//} 
+int main(void){
+birthday("Mykel", 15);
+birthday("Lucas", 40);
+birthday("Victoria", 19);
+birthday("Juliette", 67);
+//char* user = get_name();
+//birthday(user, 5);
+//int addition = add(5,4);
+//printf("%d\n", addition);
+//printf("%d\n", add(50,37));
+
+return 0;
+}
+
+
+
+//What a function is
+///storage cotainers for actions that we can use over and over again
+//you do not write functions inside of other functions.
+
+//Why we use functions
+// because they make things easier
+
+//How to write a function in C
+//you set a paremeter specifically inside the function
+
+//What are arguments and parameters?
+
+
+//How do arguments and parameters work together?
+
+
+//How to use parameters and arguments in python
+
+
+//What are return statements?
+
+
+//How do return statements change how you define a function in C?
+
+
+
+//What do return statements do with the information?
