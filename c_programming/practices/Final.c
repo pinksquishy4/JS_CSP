945dfc0b05d0fd9428626c51230269f64990d5f9
pinksquishy4 <juliette.santacruz@ucas-edu.net>
2025-10-22 02:04:03 +0000

    did some of the project

---
+++ c_programming/practices/Final.c
@@ -0,0 +1,20 @@
+//JS 6th C final project
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void){
+ if (strcmp(word, output) == 0) {
+    printf("Yes that is the word! congratulations. Rerun code to play another round.\n");
+    break;
+} else {
+    continue;
+}
+
+
+
+
+
+
+
+    return 0;
+}
